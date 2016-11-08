1d3888804fff48d1aa1a79f0fc533e38a569cd3f
philippe <philou.gautier@gmail.com>
2016-11-08 21:20:54 +0100

    initialisation de retour defonction requete a null

--- source/fonctions_bd_gase.php
+++ source/fonctions_bd_gase.php
@@ -39,7 +39,7 @@ define("DB_PREFIX", $prefix);
 // AC 02-05-2016 une seule fonction utilisant la connexion globale pour les requêtes avec gestion d'erreur
 function requete($sql) {
 	global $mysql;
-	$result = false;
+	$result = null;
 	try {
 		$result = $mysql->query($sql);
 	}
