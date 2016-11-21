2758dda62120d6521a85399335095a25bb8e4db4
Jojo144 <jojo144@girole.fr>
2016-11-21 10:44:27 +0100

    adapter backup_script pour le gase de chips (et ça marche bien)

--- backup_script.sh
+++ backup_script.sh
@@ -11,7 +11,7 @@
 
 #extract db details from config.ini file
 #!!! path to config.ini file should be ABSOLUTE
-config_file_path="/home/gase/Web/gase/config.ini"
+config_file_path="/var/www/html/gase/config.ini"
 
 
 ################
@@ -21,14 +21,18 @@ db_name=$(awk -F " = " '/^name/ {print $2}' $config_file_path)
 db_backup_directory_root=$(awk -F " = " '/^backup_directory/ {print $2}' $config_file_path)
 db_backup_directory="$db_backup_directory_root/database_backup"
 db_backup_depth=$(awk -F " = " '/^backup_depth/ {print $2}' $config_file_path)
+backup_date=`date +%Y-%m-%d_%Hh%Mmin%S`
 
+echo 'Creating backup with parameters:'
 echo $config_file_path
 echo $db_user
 echo $db_pass
+echo $db_name
 echo $db_backup_directory
+echo $backup_date
 
 #backup
-mysqldump -u $db_user -p$db_pass $db_name > $db_backup_directory/gase_db_backup_`date +%Y-%m-%d_%Hh%Mmin%S`.sql
+mysqldump -u $db_user -p$db_pass $db_name > $db_backup_directory/gase_db_backup_$backup_date.sql
 
 #delete old backup files
 num_of_backup=$(ls $db_backup_directory | grep -c gase_db_backup)
@@ -42,8 +46,8 @@ num_of_backup=$(ls $db_backup_directory | grep -c gase_db_backup)
 done
 
 #trigger grive sync
-cd $db_backup_directory_root
-grive
+# cd $db_backup_directory_root
+# grive
 
 #to restore restore
 # mysql -u $db_user -p$db_pass $db_name < dumpfilename.sql
