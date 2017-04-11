6258e87e61b0208325ce5c475cd0e88b78a3bd46
Jojo144 <jojo144@girole.fr>
2017-04-11 10:05:19 +0200

    Readme

--- Readme.md
+++ Readme.md
@@ -2,41 +2,18 @@
 
 Logiciel de gestion de GASE ou d'épicerie associative.
 
-Attention : stade expérimental. Testé principalement avec l'option "use_mail = false".
-
-
+Attention : stade expérimental. Testé principalement avec les options `use_mail = false` et `use_documents = false`.
 
 
 ## Installation
 
-1 - Installer les paquets ubuntu : apache mysql php php-mysql libapache2-mod-php
-	(notez le mot de passe root que vous donnez lors de l'installation de mysql, par exemple root)
-
-2 - Renommer les dossier contenant les sources en "gase" et le placer dans /var/www/html:
-	sudo cp -r gase-web /var/www/html/gase 
-    Et redoner les bonnes permissions (remplacer USER par le nom d'utilisateur Ubuntu):
-	sudo chown -R USER:USER /var/www/html/gase
-
-3 - Configurer correctement le fichier config.ini. Ça doit ressembler à :
-	[DB]
-	address = localhost
-	user = root
-	password = MOTDEPASSEMYSQL
-	name = gase
-
-4 - Créer les tables dans la base de données. Dans un terminal :
-	cd /var/www/html/gase
-	./restore_backup.sh
-
-5 - Aller sur http://localhost/gase/source/ et vérifier que ça roule.
-
-6 - Mettre en place des backups.
+Voir le fichier [Installation.md].
 
 
 ## Divers
 
 Pour remettre les tables à zéro :
-./restore_backup.sh
+	./restore_backup.sh
 
 Pour restaurer un backup:
-./restore_backup.sh MONBACKUP.sql
+	./restore_backup.sh MONBACKUP.sql
--- restore_backup.sh
+++ restore_backup.sh
@@ -1,20 +1,39 @@
 #!/bin/bash
 
+# Ce script restaure un backup (exemple : ./restore_backup.sh MONBACKUP.sql)
+# en utilisant la configuration de la BD donnée le fichier config.ini.
+# Si aucun argument n'est donné au script, alors il réinitialise la base.
+
+# TODO : rattraper les erreurs (au moins ne pas dire "c'est fait !")
+
+
 #extract db details from config.ini file
-#!!! path to config.ini file should be ABSOLUTE
-config_file_path="/var/www/html/gase/config.ini"
+config_file_path="./config.ini"
 
-
-################
 db_user=$(awk -F " = " '/^user/ {print $2}' $config_file_path)
 db_pass=$(awk -F " = " '/^password/ {print $2}' $config_file_path)
 db_name=$(awk -F " = " '/^name/ {print $2}' $config_file_path)
 
+
+
 # Si appelé sans argument : remise à zéro des tables
 if [ $# = 0 ]; then
-    #toto changer gasedechips en $db_name
-    echo "DROP DATABASE IF EXISTS $db_name; CREATE DATABASE $db_name DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci; USE $db_name;" | mysql -u $db_user -p$db_pass
-    mysql -u $db_user -p$db_pass  $db_name < reset_tables.mysql
+    read -r -p "Ce script va remettre à zéro la base $db_name. Voulez-vous continuer ? [y/N] " response
+    if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]
+    then
+	mysql -u $db_user -p$db_pass -e "DROP DATABASE IF EXISTS $db_name; CREATE DATABASE $db_name DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci; USE $db_name;" 2>&1 | grep -v "Using a password"
+	mysql -u $db_user -p$db_pass $db_name < reset_tables.mysql 2>&1 | grep -v "Using a password"
+	echo "C'est fait !"
+    else
+	echo "Abort."
+    fi
 else
-    mysql -u $db_user -p$db_pass $db_name < $1
+    read -r -p "Ce script va restaurer le backup $1 sur la base $db_name. Voulez-vous continuer ? [y/N] " response
+    if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]
+    then
+	mysql -u $db_user -p$db_pass $db_name < $1 2>&1 | grep -v "Using a password"
+	echo "C'est fait !"
+    else
+	echo "Abort."
+    fi
 fi
